b5e4e6f01de3244e6cd4e478233c6fd35c92c39d
xiaodabao <xiaojin1992@126.com>
2025-05-06 00:00:38 +0800

    feat: udpate

---
+++ src/three/controls.ts
@@ -0,0 +1,9 @@
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import { camera } from './camera';
+import { renderer } from './renderer';
+
+const { domElement } = renderer;
+
+// 轨道控制器
+export const controls = new OrbitControls(camera, domElement);
+controls.enableDamping = true;
