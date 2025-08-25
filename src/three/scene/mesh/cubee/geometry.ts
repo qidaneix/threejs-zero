cdd8b26a86bebb35776f0d3f59709961f1afa0c1
dabao.xiao <dabao.xiao@zyt.com>
2025-08-25 11:00:22 +0800

    feat: 燃烧心灵

---
+++ src/three/scene/mesh/cubee/geometry.ts
@@ -0,0 +1,8 @@
+import { BoxxGeometry } from "../../../geometrys/BoxxGeometry";
+
+const boxWidth = 1;
+const boxHeight = 1;
+const boxDepth = 1;
+
+// 长方体
+export const geometry = new BoxxGeometry(boxWidth, boxHeight, boxDepth);
