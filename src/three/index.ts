54ce4f4ae68b788034ace63524abd1fe20dd067d
xiaodabao <xiaojin1992@126.com>
2025-09-07 23:27:21 +0800

    all update

--- src/three/index.ts
+++ src/three/index.ts
@@ -1,29 +1,49 @@
 import { renderer } from './renderer';
 import { camera } from './camera';
-import { controls } from './controls';
 import { scene } from './scene';
-import { box } from './scene/mesh/box';
+import { orbitControls } from './controls/orbit';
+import { transformControls } from './controls/transform';
 
+export { renderer };
+
+// 键盘快捷键
+document.body.addEventListener('keydown', function (event) {
+  switch (event.key.toLowerCase()) {
+    case 't': {
+      transformControls.setMode('translate');
+      break;
+    }
+    case 'r': {
+      transformControls.setMode('rotate');
+      break;
+    }
+    case 's': {
+      transformControls.setMode('scale');
+      break;
+    }
+  }
+});
+
+// --- 窗口自适应 ---
+document.body.addEventListener('resize', function () {
+  // 更新相机的宽高比
+  camera.aspect = document.body.clientWidth / document.body.clientHeight;
+  // 更新相机的投影矩阵
+  camera.updateProjectionMatrix();
+  // 更新渲染器尺寸
+  renderer.setSize(document.body.clientWidth, document.body.clientHeight);
+});
+
+// --- 渲染循环 ---
 function animate(time: number) {
   // box.rotation.x += 10e-3;
   // box.rotation.y += 10e-3;
 
-  controls.update();
+  if (orbitControls.enabled) orbitControls.update();
+
   renderer.render(scene, camera);
 
   requestAnimationFrame(animate);
 }
 
 requestAnimationFrame(animate);
-
-// 调整窗口大小
-document.body.addEventListener('resize', function () {
-  // 更新渲染器尺寸
-  renderer.setSize(document.body.clientWidth, document.body.clientHeight);
-  // 更新相机的宽高比
-  camera.aspect = document.body.clientWidth / document.body.clientHeight;
-  // 更新相机的投影矩阵
-  camera.updateProjectionMatrix();
-});
-
-export { renderer };
