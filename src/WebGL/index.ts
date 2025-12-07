e5250205997cbb4f582e1ddd93e6d8ba027f5b26
xiaodabao <xiaojin1992@126.com>
2025-12-07 15:46:44 +0800

    HelloCanvas

--- src/WebGL/index.ts
+++ src/WebGL/index.ts
@@ -1,26 +1,4 @@
 import { initCanvas } from './init-canvas';
-import { initVertexBuffers } from './init-vertex-buffers';
-
-// Vertex shader program
-const VSHADER_SOURCE = /* glsl */ `
-  attribute vec4 a_Position;
-  attribute vec4 a_Color;
-  uniform mat4 u_ModelViewMatrix;
-  varying vec4 v_Color;
-  void main() {
-    gl_Position = u_ModelViewMatrix * a_Position;
-    v_Color = a_Color;
-  }
-`;
-
-// Fragment shader program
-const FSHADER_SOURCE = /* glsl */ `
-  precision mediump float;
-  varying vec4 v_Color;
-  void main() {
-    gl_FragColor = v_Color;
-  }
-`;
 
 export function main(container: HTMLDivElement) {
   const ele = initCanvas(container);
@@ -31,46 +9,9 @@ export function main(container: HTMLDivElement) {
     return;
   }
 
-  // Initialize shaders
-  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
-    console.log('Failed to intialize shaders.');
-    return;
-  }
-
-  // set the vertex coordinates and color
-  const n = initVertexBuffers(gl);
-  if (n < 0) {
-    console.log('Failed to set the positions of the vertices');
-    return;
-  }
-
   // specify the color for clearing <canvas>
   gl.clearColor(0, 0, 0, 1);
 
-  // get the storage location of u_ViewMatrix
-  const u_ModelViewMatrix = gl.getUniformLocation(gl.program, 'u_ModelViewMatrix');
-  if (!u_ModelViewMatrix) {
-    console.log('Failed to get the storage location of u_ModelViewMatrix');
-    return;
-  }
-
-  // set the matrix to be used for to set the camera view
-  const viewMatrix = new Matrix4();
-  viewMatrix.setLookAt(0.2, 0.25, 0.25, 0, 0, 0, 0, 1, 0);
-
-  // calculate matrix for rotate
-  const modelMatrix = new Matrix4();
-  modelMatrix.setRotate(-10, 0, 0, 1); // rotate
-
-  // Multiply model matrix to view matrix
-  const modelViewMatrix = viewMatrix.multiply(modelMatrix);
-
-  // Pass the model view projection matrix
-  gl.uniformMatrix4fv(u_ModelViewMatrix, false, modelViewMatrix.elements);
-
   // clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
-
-  // draw the triangle
-  gl.drawArrays(gl.TRIANGLES, 0, n);
 }
