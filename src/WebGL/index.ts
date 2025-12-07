3f2ef81bc5e5d5915a6751c0a70132935fce219b
xiaodabao <xiaojin1992@126.com>
2025-12-07 23:51:12 +0800

    TranslatedTriangle

--- src/WebGL/index.ts
+++ src/WebGL/index.ts
@@ -26,6 +26,19 @@ export function main(container: HTMLDivElement) {
     return;
   }
 
+  // pass the translation distance to the vertex shader
+  const u_Translation = gl.getUniformLocation(gl.program, 'u_Translation');
+  if (!u_Translation) {
+    console.log('Failed to get the storage location of u_Translation');
+    return;
+  }
+
+  // The translation distance for x, y, and z direction
+  const Tx = 0.5,
+    Ty = 0.5,
+    Tz = 0.0;
+  gl.uniform4f(u_Translation, Tx, Ty, Tz, 0);
+
   // specify the color for clearing <canvas>
   gl.clearColor(0, 0, 0, 1);
 
@@ -33,12 +46,12 @@ export function main(container: HTMLDivElement) {
   gl.clear(gl.COLOR_BUFFER_BIT);
 
   // Draw the rectangle
-  gl.drawArrays(gl.TRIANGLE_FAN, 0, n);
+  gl.drawArrays(gl.TRIANGLES, 0, n);
 }
 
 function initVertexBuffers(gl: WebGL2RenderingContext) {
-  const vertices = new Float32Array([-0.5, 0.5, -0.5, -0.5, 0.5, 0.5, 0.5, -0.5]);
-  const n = 4; // The number of vertices
+  const vertices = new Float32Array([0, 0.5, -0.5, -0.5, 0.5, -0.5]);
+  const n = 3; // The number of vertices
 
   // create a buffer object
   const vertexBuffer = gl.createBuffer();
--- src/WebGL/vShanderSource.ts
+++ src/WebGL/vShanderSource.ts
@@ -1,7 +1,8 @@
 // Vertex shader program
 export const VSHADER_SOURCE = /* glsl */ `
 attribute vec4 a_Position;
+uniform vec4 u_Translation;
 void main() {
-  gl_Position = a_Position;
+  gl_Position = a_Position + u_Translation;
 }
 `;
