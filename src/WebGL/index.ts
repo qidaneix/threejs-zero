c9ae73fd29c57c83cf75941b25f678200ff671c8
xiaodabao <xiaojin1992@126.com>
2025-12-07 23:21:00 +0800

    HelloQuad_FAN

--- src/WebGL/index.ts
+++ src/WebGL/index.ts
@@ -33,7 +33,7 @@ export function main(container: HTMLDivElement) {
   gl.clear(gl.COLOR_BUFFER_BIT);
 
   // Draw the rectangle
-  gl.drawArrays(gl.TRIANGLE_STRIP, 0, n);
+  gl.drawArrays(gl.TRIANGLE_FAN, 0, n);
 }
 
 function initVertexBuffers(gl: WebGL2RenderingContext) {
