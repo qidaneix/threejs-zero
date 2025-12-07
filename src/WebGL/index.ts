61171cc3892bc75ee6ab475a07fdb1efb764b07a
xiaodabao <xiaojin1992@126.com>
2025-12-07 21:14:15 +0800

    ClickedPoints

--- src/WebGL/index.ts
+++ src/WebGL/index.ts
@@ -26,15 +26,43 @@ export function main(container: HTMLDivElement) {
     return;
   }
 
-  // Pass vertex position to attribute variable
-  gl.vertexAttrib3f(a_Position, 0, 0, 0);
+  ele.addEventListener('mousedown', function (ev) {
+    click(ev, gl, ele, a_Position);
+  });
 
   // specify the color for clearing <canvas>
   gl.clearColor(0, 0, 0, 1);
 
   // clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
+}
 
-  // draw a point
-  gl.drawArrays(gl.POINTS, 0, 1);
+const g_points: number[] = []; // The array for the position of a mouse press
+function click(
+  ev: MouseEvent,
+  gl: WebGL2RenderingContext,
+  canvas: HTMLCanvasElement,
+  a_Position: number,
+) {
+  const x = ev.clientX; // x coordinate of a mouse pointer
+  const y = ev.clientY; // y coordinate of a mouse pointer
+  const rect = ev.target.getBoundingClientRect();
+
+  const standX = (x - rect.left - canvas.width / 2) / (canvas.width / 2);
+  const standY = (canvas.height / 2 - (y - rect.top)) / (canvas.height / 2);
+
+  // Store the coordinates to g_points array
+  g_points.push(standX);
+  g_points.push(standY);
+
+  // clear <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  for (let i = 0; i < g_points.length; i += 2) {
+    // Pass the position of a point to a_Position variable
+    gl.vertexAttrib3f(a_Position, g_points[i], g_points[i + 1], 0);
+
+    // draw a point
+    gl.drawArrays(gl.POINTS, 0, 1);
+  }
 }
