8ee4eac7605b843e62f3a524c17a4aafbb19956e
xiaodabao <xiaojin1992@126.com>
2025-12-14 22:36:57 +0800

    PerspectiveView_mvp

--- src/WebGL/index.ts
+++ src/WebGL/index.ts
@@ -29,64 +29,62 @@ export function main(container: HTMLDivElement) {
   // Specify the color for clearing <canvas>
   gl.clearColor(0, 0, 0, 1);
 
-  // get the storage location of u_ViewMatrix and u_ProjMatrix
+  // Get the storage locations of u_ModelMatrix, u_ViewMatrix, and u_ProjMatrix
+  const u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
   const u_ViewMatrix = gl.getUniformLocation(gl.program, 'u_ViewMatrix');
   const u_ProjMatrix = gl.getUniformLocation(gl.program, 'u_ProjMatrix');
-  if (!u_ViewMatrix || !u_ProjMatrix) {
-    console.log('Failed to get the storage location of u_ViewMatrix and/or u_ProjMatrix');
+  if (!u_ModelMatrix || !u_ViewMatrix || !u_ProjMatrix) {
+    console.log(
+      'Failed to Get the storage locations of u_ModelMatrix, u_ViewMatrix, and/or u_ProjMatrix',
+    );
     return;
   }
 
-  const viewMatrix = new Matrix4();
-  const projMatrix = new Matrix4();
+  const modelMatrix = new Matrix4(); // The model matrix
+  const viewMatrix = new Matrix4(); // The view matrix
+  const projMatrix = new Matrix4(); // The projection matrix
 
-  // calculate the view matrix and projection matrix
+  // Calculate the view matrix and the projection matrix
+  modelMatrix.setTranslate(0.75, 0, 0); // Translate 0.75 units along the positive x-axis
   viewMatrix.setLookAt(0, 0, 5, 0, 0, -100, 0, 1, 0);
   projMatrix.setPerspective(30, ele.width / ele.height, 1, 100);
 
-  // Pass the view and projection matrix to u_ViewMatrix, u_ProjMatrix
+  // Pass the model, view, and projection matrix to the uniform variable respectively
+  gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
   gl.uniformMatrix4fv(u_ViewMatrix, false, viewMatrix.elements);
   gl.uniformMatrix4fv(u_ProjMatrix, false, projMatrix.elements);
 
-  // Clear <canvas>
-  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.clear(gl.COLOR_BUFFER_BIT); // Clear <canvas>
 
-  // Draw the triangles
-  gl.drawArrays(gl.TRIANGLES, 0, n);
+  gl.drawArrays(gl.TRIANGLES, 0, n); // Draw the triangles
+
+  // Prepare the model matrix for another pair of triangles
+  modelMatrix.setTranslate(-0.75, 0, 0); // Translate 0.75 units along the negative x-axis
+  // Modify only the model matrix
+  gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
+
+  gl.drawArrays(gl.TRIANGLES, 0, n); // Draw the triangles
 }
 
 function initVertexBuffers(gl: WebGL2RenderingContext) {
   /* prettier-ignore */
   const verticesColors = new Float32Array([
-     // Three triangles on the right side
-    0.75,  1.0,  -4.0,  0.4,  1.0,  0.4, // The back green one
-    0.25, -1.0,  -4.0,  0.4,  1.0,  0.4,
-    1.25, -1.0,  -4.0,  1.0,  0.4,  0.4,
+    // Vertex coordinates and color
+     0.0,  1.0,  -4.0,  0.4,  1.0,  0.4, // The back green one
+    -0.5, -1.0,  -4.0,  0.4,  1.0,  0.4,
+     0.5, -1.0,  -4.0,  1.0,  0.4,  0.4,
 
-    0.75,  1.0,  -2.0,  1.0,  1.0,  0.4, // The middle yellow one
-    0.25, -1.0,  -2.0,  1.0,  1.0,  0.4,
-    1.25, -1.0,  -2.0,  1.0,  0.4,  0.4,
+     0.0,  1.0,  -2.0,  1.0,  1.0,  0.4, // The middle yellow one
+    -0.5, -1.0,  -2.0,  1.0,  1.0,  0.4,
+     0.5, -1.0,  -2.0,  1.0,  0.4,  0.4,
 
-    0.75,  1.0,   0.0,  0.4,  0.4,  1.0,  // The front blue one
-    0.25, -1.0,   0.0,  0.4,  0.4,  1.0,
-    1.25, -1.0,   0.0,  1.0,  0.4,  0.4,
-
-    // Three triangles on the left side
-   -0.75,  1.0,  -4.0,  0.4,  1.0,  0.4, // The back green one
-   -1.25, -1.0,  -4.0,  0.4,  1.0,  0.4,
-   -0.25, -1.0,  -4.0,  1.0,  0.4,  0.4,
-
-   -0.75,  1.0,  -2.0,  1.0,  1.0,  0.4, // The middle yellow one
-   -1.25, -1.0,  -2.0,  1.0,  1.0,  0.4,
-   -0.25, -1.0,  -2.0,  1.0,  0.4,  0.4,
-
-   -0.75,  1.0,   0.0,  0.4,  0.4,  1.0,  // The front blue one
-   -1.25, -1.0,   0.0,  0.4,  0.4,  1.0,
-   -0.25, -1.0,   0.0,  1.0,  0.4,  0.4,
+     0.0,  1.0,   0.0,  0.4,  0.4,  1.0,  // The front blue one
+    -0.5, -1.0,   0.0,  0.4,  0.4,  1.0,
+     0.5, -1.0,   0.0,  1.0,  0.4,  0.4,
   ])
   /* prettier-ignore */
 
-  const n = 18;
+  const n = 9;
 
   // Create a buffer object
   const vertexColorBuffer = gl.createBuffer();
--- src/WebGL/vShanderSource.ts
+++ src/WebGL/vShanderSource.ts
@@ -2,11 +2,12 @@
 export const VSHADER_SOURCE = /* glsl */ `
 attribute vec4 a_Position;
 attribute vec4 a_Color;
+uniform mat4 u_ModelMatrix;
 uniform mat4 u_ViewMatrix;
 uniform mat4 u_ProjMatrix;
 varying vec4 v_Color;
 void main() {
-  gl_Position = u_ProjMatrix * u_ViewMatrix * a_Position;
+  gl_Position = u_ProjMatrix * u_ViewMatrix * u_ModelMatrix * a_Position;
   v_Color = a_Color;
 }
 `;
