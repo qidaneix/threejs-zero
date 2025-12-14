7c57b5d487d3a4e3c1052f1076126dd84cd5e290
xiaodabao <xiaojin1992@126.com>
2025-12-14 22:12:49 +0800

    PerspectiveView

--- src/WebGL/index.ts
+++ src/WebGL/index.ts
@@ -15,56 +15,78 @@ export function main(container: HTMLDivElement) {
 
   // Initialize shaders
   if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
-    console.log('failed to initialize shaders.');
+    console.log('Failed to initialize shaders.');
     return;
   }
 
   // Set the vertex coordinates and color (the blue triangle is in the front)
   const n = initVertexBuffers(gl);
   if (n < 0) {
-    console.log('Failed to set the positions of the vertices');
+    console.log('Failed to set the vertex information');
     return;
   }
 
   // Specify the color for clearing <canvas>
   gl.clearColor(0, 0, 0, 1);
 
-  // get the storage location of u_ProjMatrix
+  // get the storage location of u_ViewMatrix and u_ProjMatrix
+  const u_ViewMatrix = gl.getUniformLocation(gl.program, 'u_ViewMatrix');
   const u_ProjMatrix = gl.getUniformLocation(gl.program, 'u_ProjMatrix');
-  if (!u_ProjMatrix) {
-    console.log('Failed to get the storage location of u_ProjMatrix');
+  if (!u_ViewMatrix || !u_ProjMatrix) {
+    console.log('Failed to get the storage location of u_ViewMatrix and/or u_ProjMatrix');
     return;
   }
 
-  // Create the matrix to set the eye point, and the line of sight
+  const viewMatrix = new Matrix4();
   const projMatrix = new Matrix4();
-  // Register the event handler to be called on key press
-  window.addEventListener('keydown', function (event) {
-    keydown(event, gl, n, u_ProjMatrix, projMatrix);
-  });
 
-  draw(gl, n, u_ProjMatrix, projMatrix); // Draw
+  // calculate the view matrix and projection matrix
+  viewMatrix.setLookAt(0, 0, 5, 0, 0, -100, 0, 1, 0);
+  projMatrix.setPerspective(30, ele.width / ele.height, 1, 100);
+
+  // Pass the view and projection matrix to u_ViewMatrix, u_ProjMatrix
+  gl.uniformMatrix4fv(u_ViewMatrix, false, viewMatrix.elements);
+  gl.uniformMatrix4fv(u_ProjMatrix, false, projMatrix.elements);
+
+  // Clear <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  // Draw the triangles
+  gl.drawArrays(gl.TRIANGLES, 0, n);
 }
 
 function initVertexBuffers(gl: WebGL2RenderingContext) {
   /* prettier-ignore */
   const verticesColors = new Float32Array([
-    // Vertex coordinates and color
-     0.0,  0.6,  -0.4,  0.4,  1.0,  0.4, // The back green one
-    -0.5, -0.4,  -0.4,  0.4,  1.0,  0.4,
-     0.5, -0.4,  -0.4,  1.0,  0.4,  0.4,
+     // Three triangles on the right side
+    0.75,  1.0,  -4.0,  0.4,  1.0,  0.4, // The back green one
+    0.25, -1.0,  -4.0,  0.4,  1.0,  0.4,
+    1.25, -1.0,  -4.0,  1.0,  0.4,  0.4,
 
-     0.5,  0.4,  -0.2,  1.0,  0.4,  0.4, // The middle yellow one
-    -0.5,  0.4,  -0.2,  1.0,  1.0,  0.4,
-     0.0, -0.6,  -0.2,  1.0,  1.0,  0.4,
+    0.75,  1.0,  -2.0,  1.0,  1.0,  0.4, // The middle yellow one
+    0.25, -1.0,  -2.0,  1.0,  1.0,  0.4,
+    1.25, -1.0,  -2.0,  1.0,  0.4,  0.4,
 
-     0.0,  0.5,   0.0,  0.4,  0.4,  1.0, // The front blue one
-    -0.5, -0.5,   0.0,  0.4,  0.4,  1.0,
-     0.5, -0.5,   0.0,  1.0,  0.4,  0.4,
+    0.75,  1.0,   0.0,  0.4,  0.4,  1.0,  // The front blue one
+    0.25, -1.0,   0.0,  0.4,  0.4,  1.0,
+    1.25, -1.0,   0.0,  1.0,  0.4,  0.4,
+
+    // Three triangles on the left side
+   -0.75,  1.0,  -4.0,  0.4,  1.0,  0.4, // The back green one
+   -1.25, -1.0,  -4.0,  0.4,  1.0,  0.4,
+   -0.25, -1.0,  -4.0,  1.0,  0.4,  0.4,
+
+   -0.75,  1.0,  -2.0,  1.0,  1.0,  0.4, // The middle yellow one
+   -1.25, -1.0,  -2.0,  1.0,  1.0,  0.4,
+   -0.25, -1.0,  -2.0,  1.0,  0.4,  0.4,
+
+   -0.75,  1.0,   0.0,  0.4,  0.4,  1.0,  // The front blue one
+   -1.25, -1.0,   0.0,  0.4,  0.4,  1.0,
+   -0.25, -1.0,   0.0,  1.0,  0.4,  0.4,
   ])
   /* prettier-ignore */
 
-  const n = 9;
+  const n = 18;
 
   // Create a buffer object
   const vertexColorBuffer = gl.createBuffer();
@@ -78,6 +100,7 @@ function initVertexBuffers(gl: WebGL2RenderingContext) {
   gl.bufferData(gl.ARRAY_BUFFER, verticesColors, gl.STATIC_DRAW);
 
   const FSIZE = verticesColors.BYTES_PER_ELEMENT;
+
   // Assign the buffer object to a_Position and enable the assignment
   const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
   if (a_Position < 0) {
@@ -100,63 +123,4 @@ function initVertexBuffers(gl: WebGL2RenderingContext) {
   gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
   return n;
-}
-
-// The distances to the near and far clipping plane (hundredfold of the real value)
-let g_near = 0.0,
-  g_far = 0.5;
-function keydown(
-  event: KeyboardEvent,
-  gl: WebGL2RenderingContext,
-  n: number,
-  u_ProjMatrix: WebGLUniformLocation,
-  projMatrix: Matrix4,
-) {
-  switch (event.keyCode) {
-    case 39: {
-      // The right arrow key was pressed
-      g_near += 0.01;
-      break;
-    }
-    case 37: {
-      // The left arrow key was pressed
-      g_near -= 0.01;
-      break;
-    }
-    case 38: {
-      // The up arrow key was pressed
-      g_far += 0.01;
-      break;
-    }
-    case 40: {
-      // The down arrow key was pressed
-      g_far -= 0.01;
-      break;
-    }
-    default:
-      return;
-  }
-
-  draw(gl, n, u_ProjMatrix, projMatrix);
-}
-
-function draw(
-  gl: WebGL2RenderingContext,
-  n: number,
-  u_ProjMatrix: WebGLUniformLocation,
-  projMatrix: Matrix4,
-) {
-  // Specify the viewing volume
-  projMatrix.setOrtho(-0.3, 0.3, -1.0, 1.0, g_near, g_far);
-
-  // Pass the view projection matrix
-  gl.uniformMatrix4fv(u_ProjMatrix, false, projMatrix.elements);
-
-  gl.clear(gl.COLOR_BUFFER_BIT); // Clear <canvas>
-
-  // Display the current near and far values
-  console.log(
-    'near: ' + Math.round(g_near * 100) / 100 + ', far: ' + Math.round(g_far * 100) / 100,
-  );
-  gl.drawArrays(gl.TRIANGLES, 0, n); // Draw the triangle
 }
