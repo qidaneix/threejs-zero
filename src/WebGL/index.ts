b540b044ecb7abb0c3580d92b803fe087ed46ed4
xiaodabao <xiaojin1992@126.com>
2025-12-15 00:41:47 +0800

    ColoredCube

--- src/WebGL/index.ts
+++ src/WebGL/index.ts
@@ -69,66 +69,82 @@ function initVertexBuffers(gl: WebGL2RenderingContext) {
   //  v2------v3
 
   /* prettier-ignore */
-  const verticesColors = new Float32Array([
-    // Vertex coordinates and color
-     1.0,  1.0,  1.0,     1.0,  1.0,  1.0,  // v0 White
-    -1.0,  1.0,  1.0,     1.0,  0.0,  1.0,  // v1 Magenta
-    -1.0, -1.0,  1.0,     1.0,  0.0,  0.0,  // v2 Red
-     1.0, -1.0,  1.0,     1.0,  1.0,  0.0,  // v3 Yellow
-     1.0, -1.0, -1.0,     0.0,  1.0,  0.0,  // v4 Green
-     1.0,  1.0, -1.0,     0.0,  1.0,  1.0,  // v5 Cyan
-    -1.0,  1.0, -1.0,     0.0,  0.0,  1.0,  // v6 Blue
-    -1.0, -1.0, -1.0,     0.0,  0.0,  0.0   // v7 Black
-  ])
+  const vertices = new Float32Array([   // Vertex coordinates
+     1.0, 1.0, 1.0,  -1.0, 1.0, 1.0,  -1.0,-1.0, 1.0,   1.0,-1.0, 1.0,  // v0-v1-v2-v3 front
+     1.0, 1.0, 1.0,   1.0,-1.0, 1.0,   1.0,-1.0,-1.0,   1.0, 1.0,-1.0,  // v0-v3-v4-v5 right
+     1.0, 1.0, 1.0,   1.0, 1.0,-1.0,  -1.0, 1.0,-1.0,  -1.0, 1.0, 1.0,  // v0-v5-v6-v1 up
+    -1.0, 1.0, 1.0,  -1.0, 1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0,-1.0, 1.0,  // v1-v6-v7-v2 left
+    -1.0,-1.0,-1.0,   1.0,-1.0,-1.0,   1.0,-1.0, 1.0,  -1.0,-1.0, 1.0,  // v7-v4-v3-v2 down
+     1.0,-1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0, 1.0,-1.0,   1.0, 1.0,-1.0   // v4-v7-v6-v5 back
+  ]);
   /* prettier-ignore */
 
-  // Indices of the vertices
   /* prettier-ignore */
-  const indices = new Uint8Array([
-    0, 1, 2,   0, 2, 3,    // front
-    0, 3, 4,   0, 4, 5,    // right
-    0, 5, 6,   0, 6, 1,    // up
-    1, 6, 7,   1, 7, 2,    // left
-    7, 4, 3,   7, 3, 2,    // down
-    4, 7, 6,   4, 6, 5     // back
+  const colors = new Float32Array([   // Vertex coordinates
+    0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  // v0-v1-v2-v3 front(blue)
+    0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  // v0-v3-v4-v5 right(green)
+    1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  // v0-v5-v6-v1 up(red)
+    1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  // v1-v6-v7-v2 left
+    1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  // v7-v4-v3-v2 down
+    0.4, 1.0, 1.0,  0.4, 1.0, 1.0,  0.4, 1.0, 1.0,  0.4, 1.0, 1.0   // v4-v7-v6-v5 back
+  ]);
+  /* prettier-ignore */
+
+  /* prettier-ignore */
+  const indices = new Uint8Array([ // Indices of the vertices
+     0, 1, 2,   0, 2, 3,    // front
+     4, 5, 6,   4, 6, 7,    // right
+     8, 9,10,   8,10,11,    // up
+    12,13,14,  12,14,15,    // left
+    16,17,18,  16,18,19,    // down
+    20,21,22,  20,22,23     // back
  ]);
   /* prettier-ignore */
 
+  // Write the vertex coordinates and color to the buffer object
+  if (!initArrayBuffer(gl, vertices, 3, gl.FLOAT, 'a_Position')) return -1;
+  if (!initArrayBuffer(gl, colors, 3, gl.FLOAT, 'a_Color')) return -1;
+
   // Create a buffer object
-  const vertexColorBuffer = gl.createBuffer();
   const indexBuffer = gl.createBuffer();
-  if (!vertexColorBuffer || !indexBuffer) {
+  if (!indexBuffer) {
     console.log('Failed to create the buffer object');
     return -1;
   }
-
-  // Write the vertex coordinates and color to the buffer object
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, verticesColors, gl.STATIC_DRAW);
-
-  const FSIZE = verticesColors.BYTES_PER_ELEMENT;
-
-  // Assign the buffer object to a_Position and enable the assignment
-  const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-  if (a_Position < 0) {
-    console.log('Failed to get the storage location of a_Position');
-    return -1;
-  }
-  gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, FSIZE * 6, 0);
-  gl.enableVertexAttribArray(a_Position);
-
-  // Assign the buffer object to a_Color and enable the assignment
-  const a_Color = gl.getAttribLocation(gl.program, 'a_Color');
-  if (a_Color < 0) {
-    console.log('Failed to get the storage location of a_Color');
-    return -1;
-  }
-  gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE * 6, FSIZE * 3);
-  gl.enableVertexAttribArray(a_Color);
 
   // Write the indices to the buffer object
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
   gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
 
   return indices.length;
+}
+
+function initArrayBuffer(
+  gl: WebGL2RenderingContext,
+  data: Float32Array,
+  num: number,
+  type: number,
+  attribute: string,
+) {
+  const buffer = gl.createBuffer(); // Create a buffer object
+  if (!buffer) {
+    console.log('Failed to create the buffer object');
+    return -1;
+  }
+
+  // Write date into the buffer object
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+
+  // Assign the buffer object to the attribute variable
+  const a_attribute = gl.getAttribLocation(gl.program, attribute);
+  if (a_attribute < 0) {
+    console.log('Failed to get the storage location of ' + attribute);
+    return false;
+  }
+  gl.vertexAttribPointer(a_attribute, num, type, false, 0, 0);
+  // Enable the assignment of the buffer object to the attribute variable
+  gl.enableVertexAttribArray(a_attribute);
+
+  return true;
 }
