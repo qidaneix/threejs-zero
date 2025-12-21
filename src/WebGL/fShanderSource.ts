2b22858eee81416a3e2a9c790407d199ec0436a0
xiaodabao <xiaojin1992@126.com>
2025-12-21 16:15:48 +0800

    RoundedPoints

--- src/WebGL/fShanderSource.ts
+++ src/WebGL/fShanderSource.ts
@@ -3,8 +3,10 @@ export const FSHADER_SOURCE = /* glsl */ `
 #ifdef GL_ES
 precision mediump float;
 #endif
-varying vec4 v_Color;
-void main() {
-  gl_FragColor = v_Color;
+void main() { // Center coordinate is (0.5, 0.5)
+  float d = distance(gl_PointCoord, vec2(0.5, 0.5));
+  if (d < 0.5) { // Radius is 0.5
+    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+  } else { discard; }
 }
 `;
--- src/WebGL/index.ts
+++ src/WebGL/index.ts
@@ -20,210 +20,58 @@ export function main(container: HTMLDivElement) {
   }
 
   // Set the vertex information
-  const n = initVertexBuffers(gl);
+  const n = initVertexBuffer(gl);
   if (n < 0) {
     console.log('Failed to set the vertex information');
     return;
   }
 
-  // Set the clear color and enable the depth test
+  // Specify the color for clearing <canvas>
   gl.clearColor(0, 0, 0, 1);
-  gl.enable(gl.DEPTH_TEST);
 
-  // Get the storage locations of uniform variables
-  const u_MvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix');
-  const u_NormalMatrix = gl.getUniformLocation(gl.program, 'u_NormalMatrix');
-  if (!u_MvpMatrix || !u_NormalMatrix) {
-    console.log('Failed to get the storage location');
-    return;
-  }
+  // Clear <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT);
 
-  // Calculate the view projection matrix
-  const viewProjMatrix = new Matrix4();
-  viewProjMatrix.setPerspective(50, ele.width / ele.height, 1, 100);
-  viewProjMatrix.lookAt(20.0, 10.0, 30.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-
-  // Register the event handler to be called when keys are pressed
-  document.addEventListener('keydown', function (ev: KeyboardEvent) {
-    keydown(ev, gl, n, viewProjMatrix, u_MvpMatrix, u_NormalMatrix);
-  });
-
-  draw(gl, n, viewProjMatrix, u_MvpMatrix, u_NormalMatrix); // Draw the robot arm
+  // Draw three points
+  gl.drawArrays(gl.POINTS, 0, n);
 }
 
-function initVertexBuffers(gl: WebGL2RenderingContext) {
-  // Vertex coordinates（a cuboid 3.0 in width, 10.0 in height, and 3.0 in length with its origin at the center of its bottom)
+function initVertexBuffer(gl: WebGL2RenderingContext) {
   /* prettier-ignore */
-  const vertices = new Float32Array([
-    1.5, 10.0, 1.5, -1.5, 10.0, 1.5, -1.5,  0.0, 1.5,  1.5,  0.0, 1.5, // v0-v1-v2-v3 front
-    1.5, 10.0, 1.5,  1.5,  0.0, 1.5,  1.5,  0.0,-1.5,  1.5, 10.0,-1.5, // v0-v3-v4-v5 right
-    1.5, 10.0, 1.5,  1.5, 10.0,-1.5, -1.5, 10.0,-1.5, -1.5, 10.0, 1.5, // v0-v5-v6-v1 up
-   -1.5, 10.0, 1.5, -1.5, 10.0,-1.5, -1.5,  0.0,-1.5, -1.5,  0.0, 1.5, // v1-v6-v7-v2 left
-   -1.5,  0.0,-1.5,  1.5,  0.0,-1.5,  1.5,  0.0, 1.5, -1.5,  0.0, 1.5, // v7-v4-v3-v2 down
-    1.5,  0.0,-1.5, -1.5,  0.0,-1.5, -1.5, 10.0,-1.5,  1.5, 10.0,-1.5  // v4-v7-v6-v5 back
-  ]);
+  const vertices = new Float32Array([ // Vertices
+    0.0,  0.5,  0.0,
+   -0.5, -0.5,  0.0,
+    0.5, -0.5,  0.0,
+  ])
   /* prettier-ignore */
 
-  // Normal
-  /* prettier-ignore */
-  const normals = new Float32Array([
-    0.0, 0.0, 1.0,  0.0, 0.0, 1.0,  0.0, 0.0, 1.0,  0.0, 0.0, 1.0, // v0-v1-v2-v3 front
-    1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  1.0, 0.0, 0.0, // v0-v3-v4-v5 right
-    0.0, 1.0, 0.0,  0.0, 1.0, 0.0,  0.0, 1.0, 0.0,  0.0, 1.0, 0.0, // v0-v5-v6-v1 up
-   -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, // v1-v6-v7-v2 left
-    0.0,-1.0, 0.0,  0.0,-1.0, 0.0,  0.0,-1.0, 0.0,  0.0,-1.0, 0.0, // v7-v4-v3-v2 down
-    0.0, 0.0,-1.0,  0.0, 0.0,-1.0,  0.0, 0.0,-1.0,  0.0, 0.0,-1.0  // v4-v7-v6-v5 back
-  ]);
-  /* prettier-ignore */
+  const n = 3; // The number of vertices
 
-  // Indices of the vertices
-  /* prettier-ignore */
-  const indices = new Uint8Array([
-     0, 1, 2,   0, 2, 3,    // front
-     4, 5, 6,   4, 6, 7,    // right
-     8, 9,10,   8,10,11,    // up
-    12,13,14,  12,14,15,    // left
-    16,17,18,  16,18,19,    // down
-    20,21,22,  20,22,23     // back
- ]);
-  /* prettier-ignore */
+  // Create a buffer object
+  const vertexBuffer = gl.createBuffer();
+  if (!vertexBuffer) {
+    console.log('Failed to create the buffer object');
+    return -1;
+  }
 
-  // Write the vertex property to buffers (coordinates and normals)
-  if (!initArrayBuffer(gl, 'a_Position', vertices, gl.FLOAT, 3)) return -1;
-  if (!initArrayBuffer(gl, 'a_Normal', normals, gl.FLOAT, 3)) return -1;
+  // Bind the vertex buffer
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  // Write date into the buffer object
+  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+  // Assign the buffer object to the attribute variable
+  const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  if (a_Position < 0) {
+    console.log('Failed to get the storage location of a_Position');
+    return -1;
+  }
+  gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
 
   // Unbind the buffer object
   gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
-  // Write the indices to the buffer object
-  const indexBuffer = gl.createBuffer();
-  if (!indexBuffer) {
-    console.log('Failed to create the buffer object');
-    return -1;
-  }
+  // Enable the assignment to a_Position variable
+  gl.enableVertexAttribArray(a_Position);
 
-  // Write the indices to the buffer object
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
-
-  return indices.length;
-}
-
-function initArrayBuffer(
-  gl: WebGL2RenderingContext,
-  attribute: string,
-  data: Float32Array,
-  type: number,
-  num: number,
-) {
-  const buffer = gl.createBuffer(); // Create a buffer object
-  if (!buffer) {
-    console.log('Failed to create the buffer object');
-    return -1;
-  }
-
-  // Write date into the buffer object
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-  gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
-
-  // Assign the buffer object to the attribute variable
-  const a_attribute = gl.getAttribLocation(gl.program, attribute);
-  if (a_attribute < 0) {
-    console.log('Failed to get the storage location of ' + attribute);
-    return false;
-  }
-  gl.vertexAttribPointer(a_attribute, num, type, false, 0, 0);
-  // Enable the assignment of the buffer object to the attribute variable
-  gl.enableVertexAttribArray(a_attribute);
-
-  return true;
-}
-
-const ANGLE_STEP = 3.0; // The increments of rotation angle (degree)
-let g_arm1Angle = -90.0; // The rotation angle of arm1 (degree)
-let g_joint1Angle = 0.0; // The rotation angle of joint1 (degree)
-
-function keydown(
-  ev: KeyboardEvent,
-  gl: WebGL2RenderingContext,
-  n: number,
-  viewProjMatrix: Matrix4,
-  u_MvpMatrix: WebGLUniformLocation,
-  u_NormalMatrix: WebGLUniformLocation,
-) {
-  switch (ev.keyCode) {
-    case 38: {
-      // Up arrow key -> the positive rotation of joint1 around the z-axis
-      if (g_joint1Angle < 135.0) g_joint1Angle += ANGLE_STEP;
-      break;
-    }
-    case 40: {
-      // Down arrow key -> the negative rotation of joint1 around the z-axis
-      if (g_joint1Angle > -135.0) g_joint1Angle -= ANGLE_STEP;
-      break;
-    }
-    case 39: {
-      // Right arrow key -> the positive rotation of arm1 around the y-axis
-      g_arm1Angle = (g_arm1Angle + ANGLE_STEP) % 360;
-      break;
-    }
-    case 37: {
-      // Left arrow key -> the negative rotation of arm1 around the y-axis
-      g_arm1Angle = (g_arm1Angle - ANGLE_STEP) % 360;
-      break;
-    }
-    default:
-      return;
-  }
-
-  draw(gl, n, viewProjMatrix, u_MvpMatrix, u_NormalMatrix);
-}
-
-// Coordinate transformation matrix
-const g_modelMatrix = new Matrix4();
-const g_mvpMatrix = new Matrix4();
-
-function draw(
-  gl: WebGL2RenderingContext,
-  n: number,
-  viewProjMatrix: Matrix4,
-  u_MvpMatrix: WebGLUniformLocation,
-  u_NormalMatrix: WebGLUniformLocation,
-) {
-  // Clear color and depth buffer
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-  // Arm1
-  const armLength = 10.0; // Length of arm1
-  g_modelMatrix.setTranslate(0.0, -12.0, 0.0);
-  g_modelMatrix.rotate(g_arm1Angle, 0.0, 1.0, 0.0); // Rotate around the y-axis
-  drawBox(gl, n, viewProjMatrix, u_MvpMatrix, u_NormalMatrix); // Draw
-
-  // Arm2
-  g_modelMatrix.translate(0.0, armLength, 0.0); // Move to joint1
-  g_modelMatrix.rotate(g_joint1Angle, 0.0, 0.0, 1.0); // Rotate around the z-axis
-  g_modelMatrix.scale(1.3, 1.0, 1.3);
-  drawBox(gl, n, viewProjMatrix, u_MvpMatrix, u_NormalMatrix); // Draw
-}
-
-const g_normalMatrix = new Matrix4();
-
-// Draw the cube
-function drawBox(
-  gl: WebGL2RenderingContext,
-  n: number,
-  viewProjMatrix: Matrix4,
-  u_MvpMatrix: WebGLUniformLocation,
-  u_NormalMatrix: WebGLUniformLocation,
-) {
-  // Calculate the model view project matrix and pass it to u_MvpMatrix
-  g_mvpMatrix.set(viewProjMatrix);
-  g_mvpMatrix.multiply(g_modelMatrix);
-  gl.uniformMatrix4fv(u_MvpMatrix, false, g_mvpMatrix.elements);
-  // Calculate the normal transformation matrix and pass it to u_NormalMatrix
-  g_normalMatrix.setInverseOf(g_modelMatrix);
-  g_normalMatrix.transpose();
-  gl.uniformMatrix4fv(u_NormalMatrix, false, g_normalMatrix.elements);
-  // Draw
-  gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);
+  return n;
 }
