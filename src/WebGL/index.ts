990672219076b9eea293d364b1d37c67ded446da
xiaodabao <xiaojin1992@126.com>
2025-12-28 17:05:49 +0800

    MultiAttributeSize_Interleaved

--- src/WebGL/index.ts
+++ src/WebGL/index.ts
@@ -19,7 +19,7 @@ export function main(container: HTMLDivElement) {
     return;
   }
 
-  // Set the vertex information
+  // Set vertex coordinates and point sizes
   const n = initVertexBuffer(gl);
   if (n < 0) {
     console.log('Failed to set the positions of the vertices');
@@ -29,7 +29,7 @@ export function main(container: HTMLDivElement) {
   // Specify the color for clearing <canvas>
   gl.clearColor(0, 0, 0, 1);
 
-  // Clear color and depth buffer
+  // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
 
   // Draw three points
@@ -38,48 +38,41 @@ export function main(container: HTMLDivElement) {
 
 function initVertexBuffer(gl: WebGL2RenderingContext) {
   /* prettier-ignore */
-  const vertices = new Float32Array([ // Vertex coordinates
-     0.0, 0.5,  -0.5, -0.5,  0.5, -0.5,
+  const vertexSizes = new Float32Array([
+    // Coordinate and size of points
+     0.0, 0.5, 10, // the 1st point
+    -0.5, -0.5, 20, // the 2nd point
+     0.5, -0.5, 30, // the 3rd point
   ])
   /* prettier-ignore */
 
-  /* prettier-ignore */
-  const pointSize = new Float32Array([ // Point sizes
-    10, 20, 30,
-  ])
-  /* prettier-ignore */
-
-  const n = 3;
+  const n = 3; // The number of vertices
+  const FSIZE = vertexSizes.BYTES_PER_ELEMENT;
 
   // Create a buffer object
-  const vertexBuffer = gl.createBuffer();
-  const pointSizeBuffer = gl.createBuffer();
-  if (!vertexBuffer || !pointSizeBuffer) {
+  const buffer = gl.createBuffer();
+  if (!buffer) {
     console.log('Failed to create the buffer object');
     return -1;
   }
 
-  // Write vertex coordinates to the buffer object and enable it
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-  const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-  if (a_Position < 0) {
-    console.log('Failed to get the storage location of a_Position');
-    return -1;
-  }
-  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(a_Position);
+  // Bind the buffer object to target
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, vertexSizes, gl.STATIC_DRAW);
 
-  // Bind the point size buffer object to target
-  gl.bindBuffer(gl.ARRAY_BUFFER, pointSizeBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, pointSize, gl.STATIC_DRAW);
+  //Get the storage location of a_Position and a_PointSize
+  const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
   const a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
-  if (a_PointSize < 0) {
-    console.log('Failed to get the storage location of a_PointSize');
+  if (a_Position < 0 || a_PointSize < 0) {
+    console.log('Failed to get the storage location of a_Position or a_PointSize');
     return -1;
   }
-  gl.vertexAttribPointer(a_PointSize, 1, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(a_PointSize);
+
+  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 3, 0);
+  gl.enableVertexAttribArray(a_Position); // Enable the assignment of the buffer object
+
+  gl.vertexAttribPointer(a_PointSize, 1, gl.FLOAT, false, FSIZE * 3, FSIZE * 2);
+  gl.enableVertexAttribArray(a_PointSize); // Enable buffer allocation
 
   // Unbind the buffer object
   gl.bindBuffer(gl.ARRAY_BUFFER, null);
